1e710ecc9001fd4a7a383b983d784bcf58b5611e
Mahmood Garibov <mahmood.garibov@gmail.com>
2021-12-05 17:00:55 +0400

    Developer exception for different host env is updated

--- backend/Web/Startup.cs
+++ backend/Web/Startup.cs
@@ -235,7 +235,7 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
