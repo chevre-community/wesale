63a7d71f10a05db17ebbe64e3bd0ef10d584419a
Mahmood Garibov <mahmood.garibov@gmail.com>
2021-12-20 12:47:11 +0400

    Migrate database in API eliminated

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -24,7 +24,7 @@ namespace API
             {
                 var services = scope.ServiceProvider;
                 var context = scope.ServiceProvider.GetService<WeSaleContext>();
-                context.Database.Migrate();
+                //context.Database.Migrate();
 
                 var userService = scope.ServiceProvider.GetService<IUserService>();
                 var roleService = scope.ServiceProvider.GetService<IRoleService>();
